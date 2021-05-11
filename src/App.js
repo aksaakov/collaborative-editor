78826ead123a0b8ae2e4ec78f3791e0a7b3815ed
aksaakov <a.cealicov@gmail.com>
2021-05-11 20:59:59 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import logo from './logo.svg';
+import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: '100%',
+    },
+  },
+}));
+
+function App() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState('Controlled');
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="sm">
+        <form className={classes.root} noValidate autoComplete="off">
+        <div>
+          <TextField
+            id="outlined-multiline-static"
+            label="Hello World"
+            multiline
+            rows={20}
+            defaultValue="Default Value"
+            variant="outlined"
+          />
+        </div>
+     </form>
+      </Container>
+    </React.Fragment>
+  );
+}
+
+export default App;
