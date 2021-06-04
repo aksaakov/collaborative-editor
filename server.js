1b06a1116f5f3165f4495e9bf638127fe47d9780
aksaakov <a.cealicov@gmail.com>
2021-06-04 19:28:35 +0100

    updated Procfile

--- server.js
+++ server.js
@@ -17,19 +17,5 @@ app.get('/', (req, res) => {
  res.sendFile(HTML_FILE); // EDIT
 });
 app.listen(port, function () {
- console.log('App listening on port: ' + port);
-});
-
-const WebSocket = require('ws');
-
-const wss = new WebSocket.Server({ port: 1235 });
-
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(data) {
-    wss.clients.forEach(function each(client) {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(data);
-      }
-    });
-  });
+ console.log('Server is running on port: ' + port);
 });
