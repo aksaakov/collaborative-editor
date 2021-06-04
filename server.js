0b4ea9aa31d357b293c68ee839cea30f7d87b499
aksaakov <a.cealicov@gmail.com>
2021-06-04 19:42:00 +0100

    an overly simplified server.js

--- server.js
+++ server.js
@@ -1,21 +1,33 @@
-const express = require('express');
-const path = require('path'); // NEW
+// const express = require('express');
+// const path = require('path'); 
 
-const app = express();
-const port = process.env.PORT || 3001;
-const DIST_DIR = path.join(__dirname, './build'); // NEW
-const HTML_FILE = path.join(DIST_DIR, '.index.html'); // NEW
-const mockResponse = {
-  foo: 'bar',
-  bar: 'foo'
-};
-app.use(express.static(DIST_DIR)); // NEW
-app.get('/api', (req, res) => {
-  res.send(mockResponse);
-});
-app.get('/', (req, res) => {
- res.sendFile(HTML_FILE); // EDIT
-});
-app.listen(port, function () {
- console.log('Server is running on port: ' + port);
+// const app = express();
+// const port = process.env.PORT || 3001;
+// const DIST_DIR = path.join(__dirname, './build'); 
+// const HTML_FILE = path.join(DIST_DIR, '.index.html'); 
+// const mockResponse = {
+//   foo: 'bar',
+//   bar: 'foo'
+// };
+// app.use(express.static(DIST_DIR));
+// app.get('/api', (req, res) => {
+//   res.send(mockResponse);
+// });
+// app.get('/', (req, res) => {
+//  res.sendFile(HTML_FILE); // EDIT
+// });
+// app.listen(port, function () {
+//  console.log('Server is running on port: ' + port);
+// });
+
+var path = require('path');
+var express = require('express');
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.set('port', process.env.PORT || 8080);
+
+var server = app.listen(app.get('port'), function() {
+  console.log('listening on port ', server.address().port);
 });
